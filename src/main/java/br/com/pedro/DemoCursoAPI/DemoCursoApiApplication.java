3de60b59dfbf0330ad86c086e8b70ce713c8b145
PHAredes <pedro.aredes@hotmail.com>
2023-05-10 12:13:38 -0300

    Started API project with initial configs: Spring Initializr with Spring Web, Data JPA, DevTools, Actuator and PostgreSQL. Altered Maven Plugin on pom.xml to 2.1.17 version

---
+++ src/main/java/br/com/pedro/DemoCursoAPI/DemoCursoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro.DemoCursoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCursoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCursoApiApplication.class, args);
+	}
+
+}
