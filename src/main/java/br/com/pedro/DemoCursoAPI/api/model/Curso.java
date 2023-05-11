00dd7466427f06703aaea272de67cc02a7085047
PHAredes <pedro.aredes@hotmail.com>
2023-05-11 09:39:54 -0300

    Added class Curso, it has value, name and description attributes, and also a @OneToMany relation with the class Aluno. Already configured to be displayed on our database

---
+++ src/main/java/br/com/pedro/DemoCursoAPI/api/model/Curso.java
@@ -0,0 +1,72 @@
+package br.com.pedro.DemoCursoAPI.api.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+
+@JsonInclude(Include.NON_NULL)
+@Entity
+@Table(name = "curso")
+public class Curso implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String nome;
+
+    @Column(length = 300)
+    private String descricao;
+
+    @Column(nullable = false)
+    private Double valor;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "curso", fetch = FetchType.LAZY)
+    private List<Aluno> alunos;
+
+    //Geters, Seters, Equals e HashCod)
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Curso other = (Curso) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
